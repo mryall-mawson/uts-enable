e6c5f35287567446e9b9d6f656ec662992f05f71
Matt Ryall <mryall@mawsonrovers.com>
2024-02-08 15:12:40 +1100

    Fix compile error, use println instead of log

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 #[cfg(target_os = "linux")]
 use gpio::GpioOut;
 
-use log::info;
-
 fn main() {
-    let _ = env_logger::try_init(); // don't fail if called multiple times
-
     gpio_set_low(45);
     gpio_set_high(47);
     gpio_set_low(27);
@@ -16,7 +12,7 @@ pub fn gpio_set_low(pin: u16) {
     if let Ok(mut pin) = gpio::sysfs::SysFsGpioOutput::open(pin) {
         let _ = pin.set_low();
     } else {
-        error!("Can't open pin for writing: {}", pin);
+        println!("Can't open pin for writing: {}", pin);
     }
 }
 
@@ -25,16 +21,16 @@ pub fn gpio_set_high(pin: u16) {
     if let Ok(mut pin) = gpio::sysfs::SysFsGpioOutput::open(pin) {
         let _ = pin.set_high();
     } else {
-        error!("Can't open pin for writing: {}", pin);
+        println!("Can't open pin for writing: {}", pin);
     }
 }
 
 #[cfg(not(target_os = "linux"))]
 pub fn gpio_set_low(_pin: u16) {
-    info!("GPIO operations only implemented for Linux");
+    println!("GPIO operations only implemented for Linux");
 }
 
 #[cfg(not(target_os = "linux"))]
 pub fn gpio_set_high(_pin: u16) {
-    info!("GPIO operations only implemented for Linux");
+    println!("GPIO operations only implemented for Linux");
 }
